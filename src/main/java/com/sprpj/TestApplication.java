4f6990ff51ec8f4f15c224383994181609bf34cc
AlanChenlol <alan000000lol@gmail.com>
2021-07-30 17:58:23 +0900

    a

--- src/main/java/com/sprpj/TestApplication.java
+++ src/main/java/com/sprpj/TestApplication.java
@@ -3,7 +3,9 @@ package com.sprpj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 public class TestApplication {
     public static void main(String[] args){
--- src/main/java/com/sprpj/controller/TestController.java
+++ src/main/java/com/sprpj/controller/TestController.java
@@ -1,13 +1,32 @@
 package com.sprpj.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TestController {
 
     @GetMapping("/hello")
-    public String test(){
+    public String test1(){
         return "hello world springboot!";
+    }
+
+    @PostMapping("/posthello")
+    public String test2(){
+        return "post world springboot!";
+    }
+
+    @DeleteMapping("/deletehello")
+    public String test3(){
+        return "delete world springboot!";
+    }
+
+    @GetMapping("/param/{userid}/{password}")
+    public String test4(@PathVariable String userid, @PathVariable String password){
+        return "hello param get!" + userid + " : " + password;
+    }
+
+    @GetMapping("/param5")
+    public String test5(@RequestParam("userid") String abc, @RequestParam String password){
+        return "hello param get!" + abc + " : " + password;
     }
 }
