999ce7f82a683e28defc2157fe0eac3176d61ab1
チン・アラン <alan@chinarannoMacBook-Air.local>
2021-07-30 16:46:56 +0900

    Initial commit

---
+++ src/main/java/com/sprpj/TestApplication.java
@@ -0,0 +1,11 @@
+package com.sprpj;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplication {
+    public static void main(String[] args){
+        
+    }
+}
