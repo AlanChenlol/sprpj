0687f0d2f7311be81a2c14e17e5502208133075f
AlanChenlol <alan000000lol@gmail.com>
2021-07-30 16:55:02 +0900

    A

--- src/main/java/com/sprpj/TestApplication.java
+++ src/main/java/com/sprpj/TestApplication.java
@@ -1,11 +1,12 @@
 package com.sprpj;
 
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TestApplication {
     public static void main(String[] args){
-        
+        SpringApplication.run(TestApplication.class,args);
     }
 }
